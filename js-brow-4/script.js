e73280d4bc462347fd7ce82af8559f649cb04340
Andres <anglodos@gmail.com>
2023-03-30 20:45:25 -0500

    actualizacion

--- js-brow-4/script.js
+++ js-brow-4/script.js
@@ -1,89 +1,104 @@
 const menu = [
     {
-        name: 'Home',
-        link: '#'
+      name: 'Home',
+      link: '/',
+      items: [],
     },
     {
-        name: 'Productos',
-        link: '#',
-        items: [
+      name: 'About',
+      link: '/about',
+      items: [],
+    },
+    {
+      name: 'Products',
+      link: '/products',
+      items: [
+        {
+          name: 'Product 1',
+          link: '/products/1',
+          items: [],
+        },
+        {
+          name: 'Product 2',
+          link: '/products/2',
+          items: [
             {
-                name: 'Televisores',
-                link: '#',
-                items: [
-                    {
-                        name: 'LED',
-                        link: '#'
-                    },
-                    {
-                        name: 'LCD',
-                        link: '#'
-                    },
-                    {
-                        name: 'Smart TV',
-                        link: '#'
-                    }
-                ]
+              name: 'Product 2.1',
+              link: '/products/2/1',
+              items: [],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'Services',
+      link: '/services',
+      items: [
+        {
+          name: 'Service 1',
+          link: '/services/1',
+          items: [
+            {
+              name: 'Service 1.1',
+              link: '/services/1/1',
+              items: [],
+            },
+          ],
+        },
+        {
+          name: 'Service 2',
+          link: '/services/2',
+          items: [
+            {
+              name: 'Service 2.1',
+              link: '/services/2/1',
+              items: [],
             },
             {
-                name: 'Smartphones',
-                link: '#',
-                items: [
-                    {
-                        name: 'Android',
-                        link: '#'
-                    },
-                    {
-                        name: 'iOS',
-                        link: '#'
-                    },
-                    {
-                        name: 'WEB OS',
-                        link: '#'
-                    }
-
-                ]
-            }
-        ]
+              name: 'Service 2.2',
+              link: '/services/2/2',
+              items: [],
+            },
+          ],
+        },
+      ],
     },
-    {
-        name: 'Contacto',
-        link: '#'
-    }
-];
-
-const menuContainer = document.querySelector('#menu-container');
-
-function createMenu(menu, parent) {
+  ];
+  
+  const menuContainer = document.querySelector('#menu-container');
+  
+  function createMenu(menu, parent) {
     const ul = document.createElement('ul');
-
-    menu.forEach(item => {
-        const li = document.createElement('li');
-        const a = document.createElement('a');
-        a.textContent = item.name;
-        a.href = item.link;
-        li.appendChild(a);
-
-        if (item.items && item.items.length > 0) {
-            const toggle = document.createElement('span');
-            toggle.classList.add('sub-menu-toggle');
-            li.appendChild(toggle);
-
-            createMenu(item.items, li);
+  
+    menu.forEach((item) => {
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+      a.textContent = item.name;
+      a.href = item.link;
+      a.target = "_blank";
+      li.appendChild(a);
+  
+      if (item.items && item.items.length > 0) {
+        const toggle = document.createElement('span');
+        toggle.classList.add('sub-menu-toggle');
+        li.appendChild(toggle);
+  
+        createMenu(item.items, li);
+      }
+  
+      li.addEventListener('click', () => {
+        const active = document.querySelector('.active');
+        if (active) {
+          active.classList.remove('active');
         }
-
-        li.addEventListener('click', () => {
-            const active = document.querySelector('.active');
-            if (active) {
-                active.classList.remove('active');
-            }
-            li.classList.add('active');
-        });
-
-        ul.appendChild(li);
+        li.classList.add('active');
+      });
+  
+      ul.appendChild(li);
     });
-
+  
     parent.appendChild(ul);
-}
-
-createMenu(menu, menuContainer);
+  }
+  
+  createMenu(menu, menuContainer);
